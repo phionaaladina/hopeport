ffbd6aad4e01beb52a8d4a29650581a81c6e2d51
Phiona Aladina <phionaaladina@gmail.com>
2024-03-26 19:17:42 +0300

    new
    new

---
+++ script.js
@@ -0,0 +1,66 @@
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // JavaScript code for fixing navbar on scroll
+// window.addEventListener('scroll', function() {
+//     var nav = document.querySelector('nav'); // Target the <nav> element
+//     var scrollPosition = window.scrollY;
+
+//     // Change the value as needed to determine when the navbar becomes fixed
+//     var threshold = 100;
+
+//     if (scrollPosition >= threshold) {
+//         nav.classList.add('fixed-nav');
+//     } else {
+//         nav.classList.remove('fixed-nav');
+//     }
+// });
+
+
+
+
+// function toggleNav() {
+//     var navLinks = document.getElementById("navLinks");
+//     if (navLinks.style.display === "flex") {
+//         navLinks.style.display = "none";
+//     } else {
+//         navLinks.style.display = "flex";
+//     }
+// }
